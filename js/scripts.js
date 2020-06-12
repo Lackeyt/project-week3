7f54b12bfac2c4df4ecbc28c57c309e882a372ab
Tyson Lackey <lackeyt90@gmail.com>
2020-06-12 09:37:38 -0700

    create phraseConverter() not working

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,44 @@ $(document).ready(function() {
     }
     return array
   };
+
+  function phraseConverter(array) {
+    arrayTemp = []
+    for (i=0; i < array.length; i++){
+      let isThree = false
+      let isTwo = false
+      let isOne = false
+      for (n=0; n < array[i].length; n++){
+        if (array[i][n] === 3){
+          isThree = true
+          break;
+        } else if (array[i][n] === 2) {
+          isTwo = true
+        } else if (array[i][n] === 1) {
+          isOne = true
+        }
+      }
+      if (isThree === true){
+        arrayTemp.push("Won't you be my neighbor?")
+      } else if (isThree === false && isTwo === true){
+        arrayTemp.push("Boop!")
+      } else if (isThree === false && isTwo === false && isOne === true){
+        arrayTemp.push("Beep!")
+      } else {
+        arrayTemp.push(array[i])
+      }
+    }
+    return arrayTemp;
+  }
+  function main(int){
+    output = phraseConverter(arrayBuilder(int))
+    return output
+  }
 //User Interface
   $("#numberForm").submit(function() {
     event.preventDefault()
     input = $("#numberInput").val()
-    $("p.outputText").text(arrayBuilder(input).join(", "))
+    $("p.outputText").text(main(input).join(", "))
     $(".output").show()
   })
 });
