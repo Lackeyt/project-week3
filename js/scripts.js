43c6c2a1a412fb5cb39b9d6c9c6689fb77ff6bf3
Tyson Lackey <lackeyt90@gmail.com>
2020-06-12 09:09:39 -0700

    create arrayBuilder function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-  //Business Logic
-
-  //User Interface
-}
+//Business Logic
+  function arrayBuilder(int){
+    let array = []
+    let intLength = int
+    while (intLength >= 0) {
+      array.unshift(parseInt(intLength))
+      intLength -= 1
+    }
+    return array
+  };
+//User Interface
+  $("#numberForm").submit(function() {
+    event.preventDefault()
+    input = $("#numberInput").val()
+    $("p.outputText").text(arrayBuilder(input).join(", "))
+    $(".output").show()
+  })
+});
